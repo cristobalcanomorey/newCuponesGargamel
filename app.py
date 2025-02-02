d1d8e228c6b02b9f7e87179f9a2d07bb5a8ba385
TofolCano <tofolcanodam@gmail.com>
2025-02-02 02:52:07 +0100

    No va bien

--- app.py
+++ app.py
@@ -7,33 +7,80 @@ app = Flask(__name__)
 
 # Simulación de una base de datos de cupones
 coupons = {
-    "2025-01-31": {
-        "code": "CUPON0",
-        "titulo": "Cupon 0",
-        "description": "Descuento del 10%",
-        "rules":"Reglas del cupon",
-        "img":"https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif",
-        "date":"2025-01-31"
-        },
-    "2025-02-01": {
-        "code": "CUPON1",
-        "titulo": "Te deseo mucha suerte",
-        "description": """🍀🍀Te deseo que tengas muchísima suerte en el examen de catalán!! 🍀🍀
-
-Lo hablas muy bien para lo poco que llevas aprendiendo, se te dan realmente bien los idiomas. 👌
-
-Yo creo que tienes posibilidades, tú confía en ti y dale caña 💪""",
-        "rules": "Reglas del cupon",
-        "img": 'static/img/gargamelSuerte.jpeg',
-        "date": "2025-02-01"
-        },
+    # "2025-02-01": {
+    #     "code": "CUPON1",
+    #     "mensaje_desbloqueo": "¡Cupón Desbloqueado!",
+    #     "titulo": "Te deseo mucha suerte",
+    #     "description": """🍀🍀Te deseo que tengas muchísima suerte en el examen de catalán!! 🍀🍀 Lo hablas muy bien para lo poco que llevas aprendiendo, se te dan realmente bien los idiomas. 👌 Yo creo que tienes posibilidades, tú confía en ti y dale caña 💪""",
+    #     "activado": False,
+    #     "canjeable": False,
+    #     "serio": False,
+    #     "rules": "Reglas del cupon",
+    #     "img": 'static/img/gargamelSuerte.jpeg',
+    #     "date": "2025-02-01"
+    #     },
+    # "2025-02-01": {
+    #     "code": "CUPON2",
+    #     "mensaje_desbloqueo": "",
+    #     "titulo": "Necesito un poco de espacio",
+    #     "description": "Entiendo que estás pasando por un mal momento y siento muchísimo no haber respetado tus sentimientos y haber resultado ser una carga más éstos días. Si decides activar el cupón te doy el espacio que necesites, prometo respetar tu decisión y no intentaré hacer nada para recuperar nuestra amistad mientras el cupón esté activo. Independientemente de eso siempre que quieras contarme algo, si quieres un táper o si quieres que te ayude con lo que sea quiero que sepas que siempre puedes contar conmigo. Ah y cuando termine de instalarme las puertas de mi casa siempre estarán abiertas para ti, si me avisas con tiempo igual te invito a unas croquetas recién hechas y tal.",
+    #     "canjeable": True,
+    #     "serio": True,
+    #     "rules": "Puedes activarlo tantas veces como quieras",
+    #     "toggle": True,
+    #     "activado": False,
+    #     "img": "static/img/gargamelSerio.jpe", 
+    #     "date": "2025-02-02"
+    #     },
     "2025-02-02": {
         "code": "CUPON2",
-        "titulo": "Cupon 2",
-        "description": "Descuento del 10%",
-        "rules": "Reglas del cupon",
-        "img": "https://media.giphy.com/media/3o7TKSjRrfIPjeiVyM/giphy.gif", 
+        "mensaje_desbloqueo": "",
+        "titulo": "Chapa",
+        "description": """Hola Rocío... cómo te encuentras? Espero que te encuentres mejor.
+Teniendo en cuenta lo mal que lo estás pasando ahora y lo egoísta que he sido, dudo mucho que vayas a entrar hoy en ésta gilipollez de página web. Si por casualidad estás leyendo ésto, muchas gracias por todo. Nunca podré compensarte lo suficiente por todo lo que me has enseñado y todo lo que has aportado a mi felicidad, grácias a ti me dí cuenta de que sin querer estaba siendo una carga para mis padres por no hacerme cargo yo de mis propias responsabilidades, desde entonces he estado trabajando en intentar dejar de ser una carga para ellos, trabajo el cual ha culminado hoy en mi emancipación. 
+
+También me has otorgado la oportunidad de descubrir que puedo aportar a la felicidad de los demás. Sé que es algo de sentido común pero yo realmente creía que mi existencia era superflua, que de alguna forma ni yo podía aportar nada en ésta vida ni la vida podía aportarme nada a mí. Al ver que podía aportar un poquito a vuestra felicidad me he dado cuenta de que sí existo. Ésa ha sido siempre la razón por la que he hecho cosas por vosotras, mi miedo a volver a desaparecer y mis ganas de aportar todo lo posible para así poder decir que he sido capaz de aportar algo positivo en ésta vida antes de pasar a la siguiente y que no he existido sólo para gastar oxígeno.
+
+He estado reflexionando mucho y creo que tenías razón cuando dijiste que tal vez he estado confundiendo mis sentimientos… nunca antes me había sentido tan unido a nadie y nunca nadie me había aportado tanto en cuanto a crecimiento personal y autoconocimiento. Contigo he sentido que existo, tú estando tan involucrada en tu vida y siendo el centro y los cimientos de todo tu mundo y el de Itziar, en contraste conmigo que he estado viviendo siempre en una burbuja de cristal y al márgen de todo, siendo un mero espectador de una vida en la que ni pincho ni corto. Creo que sentía que para mí ya era demasiado tarde para vivir mi propia vida y posiblemente de alguna forma intentaba vivir a través de ti. Sentía que a pesar de no poder aportar a mi propia vida nada de valor tal vez sí podría aportar algo de valor a la vuestra…
+
+Creo que ésa es la razón por la que tenía tanto miedo a perderte, por eso pensaba a todas horas en ti, en tus problemas y en qué podría hacer para aportar, por eso siento tanta admiración hacia ti, por eso pensé que sólo en una relación podría aportar lo máximo posible y por eso lo confundí con amor…
+
+Creo que debería serte sincero, a pesar de decir que priorizaba nuestra amistad a ésa emoción la verdad es que ésa emoción tan intensa de anhelo por formar parte de la vida que estaba confundiendo con el amor no ha sido fácil de sobrellevar… En algunas ocasiones lo he pasado bastante mal la verdad, por patético que pueda sonar también he llegado a sentir incluso celos… No es ni medio normal, lo sé… Tenía miedo a que te marchases de mi vida y que te la llevases contigo ahora que había empezado a vivirla
+
+Pero ahora que tengo mi primer trabajo de lo que me apasiona y mi propia casa siento que tal vez, al fin y al cabo, no era demasiado tarde para mí. Tal vez yo nunca llegue a poder vivir la vida plenamente ni tenga hijos y sé que seguramente me quede algo de tara por el retraso en mi desarrollo y mi madurez, sé que me dejaré muchas cosas por vivir porque he procrastinado mucho y me he puesto a ello muy tarde… Pero aun así ahora empiezo a entender que puedo vivir mi propia vida y que no está todo perdido ni tengo que vivir indirectamente a través de nadie. Estoy entusiasmado por ello y me haría mucha ilusión que tú estuvieras a mi lado en éste proceso, me gustaría mucho que vinieses conmigo a hacer una compra grande en el súper o al Ikea a comprar unas cosillas que necesito. Me gustaría invitarte a mi casa algún día e invitarte a unas croquetas recién fritas o a un ramen…
+
+Sé que últimamente tendemos a distanciarnos cada vez más, tal vez se esté aproximando el momento en que nuestros caminos se separan. Tarde o temprano tenía que pasar… Realmente espero que éste distanciamiento sea temporal y volvamos a unirnos más pero de no ser el caso de lo único de lo que me arrepentiré, a parte de haber roto tu confianza y haberte hecho daño por supuesto, será de no haber valorado lo que tenía y de no haber podido darme cuenta antes de lo que sentía realmente. Por lo menos he llegado a darme cuenta a pesar de haberlo hecho tarde, cuando ya había permitido que se interpusiera entre nosotros y después de haberte faltado al respeto y traicionado tu confianza.
+
+Siempre te estaré eternamente agradecido por haberme enseñado todo lo que me has enseñado directa e indirectamente, por tu comprensión, tu apoyo, por haber creído en mí en algún momento y sobre todo por tu mano dura, que realmente me hacía falta para espabilar…
+
+Te quiero mucho Rocío, siempre te he querido aunque no haya sabido lo que ello significaba hasta ahora. 
+
+        """,
+        "canjeable": True,
+        "serio": True,
+        "rules": "Puedes activarlo tantas veces como quieras",
+        "toggle": True,
+        "activado": False,
+        "img": "static/img/gargamelSerio.jpe", 
         "date": "2025-02-02"
+        },
+    # "2025-02-02": {
+    #     "code": "CUPON2",
+    #     "titulo": "Espacio",
+    #     "description": "Entiendo que estás pasando por un mal momento y siento muchísimo no haber respetado tus sentimientos y haber resultado ser una carga más. Una vez activado el cupón te doy el espacio que necesites, prometo respetar tu decisión y no intentar hacer nada para recuperar nuestra amistad mientras el cupón esté activo. Independientemente de eso siempre que quieras contarme algo, si quieres comida o si quieres que te ayude con lo que sea quiero que sepas que siempre puedes contar conmigo.",
+    #     "rules": "Puedes activarlo tantas veces como quieras",
+    #     "activado": False,
+    #     "img": "static/img/gargamelSerio.jpeg", 
+    #     "date": "2025-02-02"
+    #     },
+    "2025-02-03": {
+        "code": "CUPON3",
+        "titulo": "Suministro de galletas",
+        "description": "🍪🍪Una vez activado el cupón te preparo una tanda de galletas de chocolate para dártelas la próxima vez que nos veamos 🍪🍪",
+        "rules": "Puedes activarlo tantas veces como quieras",
+        "activado": False,
+        "img": "static/img/gargamelGalletas.jpeg", 
+        "date": "2025-02-03"
         },
 }
 
@@ -44,7 +91,7 @@ unlocked_coupons = []
 def index():
     today = datetime.now().strftime('%Y-%m-%d')
 
-    # Leer la cookie correctamente
+    # Leer la cookie correctamente 
     unlocked_coupons_cookie = request.cookies.get('unlocked_coupons', '{}')
     try:
         unlocked_coupons_data = json.loads(unlocked_coupons_cookie)
@@ -59,7 +106,7 @@ def index():
         coupon = coupons[today]
         return render_template('index.html', today=today, coupon=coupon)
     else:
-        return "No hay cupones disponibles hoy."
+        return render_template('no_coupon.html')
 
 
 from flask import jsonify
@@ -75,6 +122,20 @@ def unlock_coupon(date):
             unlocked_coupons = json.loads(unlocked_coupons_cookie)
         except json.JSONDecodeError:
             unlocked_coupons = {}
+        # Original date string
+        date_str = coupon["date"]
+
+        # Define the input format
+        input_format = "%Y-%m-%d"
+
+        # Define the output format
+        output_format = "%d/%m/%Y"
+
+        # Convert string to datetime object
+        date_obj = datetime.strptime(date_str, input_format)
+
+        # Convert back to string in the new format
+        new_date_str = date_obj.strftime(output_format)
 
         # Si el cupón no está en la lista, agregarlo
         if date not in unlocked_coupons:
@@ -82,7 +143,9 @@ def unlock_coupon(date):
                 "code": coupon["code"],
                 "description": coupon["description"],
                 "img": coupon["img"],
+                "mensaje_desbloqueo": coupon["mensaje_desbloqueo"],
                 "titulo": coupon["titulo"],
+                "date": new_date_str,
                 "redeemed": False
             }
 
@@ -94,23 +157,25 @@ def unlock_coupon(date):
         return jsonify({"error": "Cupón no disponible."}), 404
 
 
-@app.route('/redeem/<code>')
+@app.route('/redeem/<code>' , methods=['POST'])
 def redeem_coupon(code):
     # Leer y decodificar la cookie
     unlocked_coupons_cookie = request.cookies.get('unlocked_coupons', '{}')
-    unlocked_coupons = json.loads(unlocked_coupons_cookie)
-    
-    # Buscar el cupón por su código
+    try:
+        unlocked_coupons = json.loads(unlocked_coupons_cookie)
+    except json.JSONDecodeError:
+        unlocked_coupons = {}
+
+    # Marcar el cupón como activado si existe
     for date, coupon in unlocked_coupons.items():
         if coupon["code"] == code:
-            # Marcar el cupón como canjeado
             coupon["redeemed"] = True
-            # Guardar la cookie actualizada
-            response = make_response(jsonify({"status": "success", "message": "Cupón canjeado correctamente."}))
-            response.set_cookie('unlocked_coupons', json.dumps(unlocked_coupons))
-            return response
+
+    # Guardar la cookie actualizada
+    response = make_response(jsonify({"message": "Cupón activado"}))
+    response.set_cookie('unlocked_coupons', json.dumps(unlocked_coupons), httponly=True, max_age=60*60*24*365)
+    return response
     
-    return jsonify({"status": "error", "message": "Cupón no encontrado."}), 404
 
 @app.route('/unlocked/<code>')
 def unlocked_coupon(code):
--- static/js/script.js
+++ static/js/script.js
@@ -1,70 +1,31 @@
-// // Obtener referencias a los elementos del DOM
-// const unlockLink = document.getElementById('unlock-link');
-// const gifContainer = document.getElementById('gif-container');
-// const secondGifContainer = document.getElementById('second-gif-container');
-// const unlockedCouponContainer = document.getElementById('unlocked-coupon-container');
-// const couponCode = document.getElementById('coupon-code');
-// const couponDescription = document.getElementById('coupon-description');
+document.addEventListener("DOMContentLoaded", function () {
+    // Verificar si localStorage ya tiene los cupones
+    if (!localStorage.getItem("unlocked_coupons")) {
+        // Leer cookies existentes
+        const cookies = document.cookie.split("; ");
+        let unlockedCoupons = {};
 
+        cookies.forEach(cookie => {
+            const [name, value] = cookie.split("=");
+            if (name.startsWith("coupon_")) {
+                try {
+                    const couponData = JSON.parse(decodeURIComponent(value));
+                    unlockedCoupons[name.replace("coupon_", "")] = couponData;
+                } catch (e) {
+                    console.error("Error al leer la cookie:", e);
+                }
+            }
+        });
 
-// unlockLink.addEventListener('click', async (event) => {
-//     event.preventDefault(); // Evitar que el enlace recargue la página
+        // Guardar en localStorage
+        if (Object.keys(unlockedCoupons).length > 0) {
+            localStorage.setItem("unlocked_coupons", JSON.stringify(unlockedCoupons));
 
-//     // Ocultar el GIF inicial y mostrar el segundo GIF
-//     gifContainer.classList.add('hidden');
-//     secondGifContainer.classList.remove('hidden');
-
-//     // Esperar 2 segundos
-//     await new Promise(resolve => setTimeout(resolve, 1700));
-
-//     // Obtener el cupón del día
-//     const today = new Date().toISOString().split('T')[0]; // Formato YYYY-MM-DD
-//     const response = await fetch(`/unlock/${today}`);
-//     if (response.ok) {
-//         const coupon = await response.json();
-
-//         // Mostrar el cupón desbloqueado
-//         couponCode.textContent = coupon.code;
-//         couponDescription.textContent = coupon.description;
-//         secondGifContainer.classList.add('hidden');
-//         unlockedCouponContainer.classList.remove('hidden');
-//     } else {
-//         alert("Error al desbloquear el cupón.");
-//     }
-
-    
-// });
-
-// function shareOnWhatsApp() {
-//     const couponCode = document.getElementById('coupon-code').textContent;
-//     const couponDescription = document.getElementById('coupon-description').textContent;
-//     const phoneNumber = "123456789";  // Reemplaza con el número de teléfono deseado
-//     const message = `¡He canjeado mi cupón! Código: ${couponCode}, Descripción: ${couponDescription}`;
-//     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-//     window.open(url, '_blank');
-// }
-
-// function shareOnWhatsApp() {
-//     const couponCode = document.getElementById('coupon-code').textContent;
-//     const couponDescription = document.getElementById('coupon-description').textContent;
-//     const phoneNumber = "609512301";  // Reemplaza con el número de teléfono deseado
-//     const message = `¡He canjeado mi cupón! Código: ${couponCode}, Descripción: ${couponDescription}`;
-//     const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
-    
-//     // Marcar el cupón como canjeado
-//     fetch(`/redeem/${couponCode}`)
-//         .then(response => response.json())
-//         .then(data => {
-//             if (data.status === "success") {
-//                 // Desactivar el botón
-//                 const whatsappButton = document.getElementById('whatsapp-button');
-//                 whatsappButton.disabled = true;
-//                 // Actualizar el estado del cupón
-//                 document.getElementById('coupon-status').textContent = "Canjeado";
-//                 // Abrir WhatsApp
-//                 window.open(url, '_blank');
-//             } else {
-//                 alert("Error al canjear el cupón.");
-//             }
-//         });
-// }
+            // Eliminar cookies antiguas
+            cookies.forEach(cookie => {
+                const [name] = cookie.split("=");
+                document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+            });
+        }
+    }
+});
